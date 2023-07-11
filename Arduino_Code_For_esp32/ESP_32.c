79d5a8de886396ac6ff85ede588264ab803c1812
Sabry Elsayed <114191115+SabryElsayed0@users.noreply.github.com>
2023-07-11 17:27:47 +0300

    Add files via upload

---
+++ Arduino_Code_For_esp32/ESP_32.c
@@ -0,0 +1,76 @@
+
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "good2";
+const char* password = "speed400";
+
+//Your IP address or domain name with URL path
+const char* serverName = "http://esp32project2.000webhostapp.com/esp-outputs-action.php?action=outputs_state&board=1";
+
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+//unsigned long timerDelay = 600000;
+// Set timer to 5 seconds (5000)
+unsigned long timerDelay = 500;
+
+void setup() {
+  Serial.begin(9600); 
+
+  WiFi.begin(ssid, password);
+  //Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+   // Serial.print(".");
+  }
+ // Serial.println("");
+  //Serial.print("Connected to WiFi network with IP Address: ");
+ // Serial.println(WiFi.localIP());
+ 
+ // Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+}
+
+void loop() {
+  //Send an HTTP POST request every 10 minutes
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
+
+
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(serverName);
+      
+      // If you need Node-RED/server authentication, insert user and password below
+      //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+      
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+      
+      if (httpResponseCode>0) {
+       // Serial.print("HTTP Response code: ");
+       // Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+                //Serial.println(payload[6]);
+                //Serial.println(payload[14]);
+       // Serial.println("led number 2"+ payload[6]);
+      }
+      else {
+        //Serial.print("Error code: ");
+        //Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    }
+    else {
+      //Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+}
